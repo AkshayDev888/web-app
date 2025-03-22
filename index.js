b93f0978272744cc2758ea16ae9b50171f4571f2
AkshayDev888 <akshaydev888@gmail.com>
2025-03-22 15:26:31 +0530

    Updated homepage message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  res.send('Welcome to My Modified Node.js App!')
 })
 
 app.listen(app.get('port'), function() {
